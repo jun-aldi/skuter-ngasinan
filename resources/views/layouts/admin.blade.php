0390a797ef15829214b1a4068a37a2f2cb3340b1
juniaraldi <aldinugraha708@gmail.com>
2022-08-18 01:56:41 +0700

    landing-page

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,6 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Skuter Ngasinan</title>
+    <link href="image/sukoharjo.png" rel="icon">
     <!-- plugins:css -->
     {{-- <link rel="stylesheet" href="vendors/feather/feather.css">
   <link rel="stylesheet" href="vendors/mdi/css/materialdesignicons.min.css">
---
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -0,0 +1,2 @@
+<img src="image/sukoharjo.png" alt="logo" style="height: 100px">
+
